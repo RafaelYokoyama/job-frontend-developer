29e017ea66226c33dcab5347e3821101c8515a20
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-10 14:06:36 -0300

    feat: add axios configurations for YouTube and Ticketmaster APIs

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const youtubeApi = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export { youtubeApi };
+
+const ticketmasterApi = axios.create({
+  baseURL: 'https://app.ticketmaster.com/discovery/v2/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export { ticketmasterApi };

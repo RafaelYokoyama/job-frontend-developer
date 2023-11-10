51a822beca4e048a69c0240477b8c88feecf336e
RafaelYokoyama <rafaelgava921@gmail.com>
2023-11-10 14:15:43 -0300

    feat: add TypeScript interface for BandDataProps

---
+++ src/types/bandData.ts
@@ -0,0 +1,15 @@
+export interface BandDataProps {
+  _embedded: {
+    attractions: {
+      name: string;
+      locale: string;
+      images: { url: string }[];
+      classifications: any;
+      externalLinks: {
+        twitter: { url: string }[];
+        facebook: { url: string }[];
+        instagram: { url: string }[];
+      };
+    }[];
+  };
+}
